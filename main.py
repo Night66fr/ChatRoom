2869fdd88b8e5fbdf7605a317fe553f2088353f9
Stan <158843492+Night66fr@users.noreply.github.com>
2026-02-22 00:06:15 +0100

    Socket start

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # ChatRoom
+import socket
+HOST = "127.0.0.1" # IP LocalHost
+PORT = 1966 # Port > 1024 (pas de droit) (sudo en dessous de 1024)
+
 def connection():
-    pass
+    #attend une connexion
+    
 
 def message():
     pass
