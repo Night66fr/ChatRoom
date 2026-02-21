91b14e6990c11b7004271d306f918bf4fa891d60
Stan <158843492+Night66fr@users.noreply.github.com>
2026-02-22 00:18:21 +0100

    Socket full test

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ import socket
 HOST = "127.0.0.1" # IP LocalHost
 PORT = 1966 # Port > 1024 (pas de droit) (sudo en dessous de 1024)
 
-def connection():
-    #attend une connexion
-    
+#attend une connexion
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as connection:
+    connection.bind((HOST, PORT))
+    connection.listen()
+    conn, addr = connection.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
+
 
 def message():
     pass
